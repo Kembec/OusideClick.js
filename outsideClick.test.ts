94ad98b40094a810a1ac785b1b226be2736327af
Kembec <account@kembec.com>
2024-11-23 17:11:48 -0500

    updated: test

--- outsideClick.test.ts
+++ outsideClick.test.ts
@@ -1,26 +1,54 @@
-import { outsideClick } from './src/index'; 
+import { outsideClick } from './src/index';
 
 describe('outsideClick', () => {
     beforeEach(() => {
         document.body.innerHTML = '<div id="testElement"></div>';
+        jest.useFakeTimers();
     });
-    it('should call onOutsideClick when clicking outside the specified element', done => {
+
+    afterEach(() => {
+        jest.useRealTimers();
+    });
+
+    it('should call onOutsideClick when clicking outside the specified element', () => {
         const elementId = 'testElement';
         const onOutsideClick = jest.fn();
 
         outsideClick(elementId, onOutsideClick);
 
-        setTimeout(() => {
-            const outsideElement = document.body;
-            const clickEvent = new MouseEvent('click', { bubbles: true });
-            outsideElement.dispatchEvent(clickEvent);
-    
-            expect(onOutsideClick).toHaveBeenCalled();
-    
-            outsideElement.dispatchEvent(clickEvent);
-            expect(onOutsideClick).toHaveBeenCalledTimes(1); 
-    
-            done();
-        }, 1000); 
+        jest.advanceTimersByTime(0);
+
+        const outsideElement = document.body;
+        const mouseDownEvent = new MouseEvent('pointerdown', { bubbles: true });
+        // @ts-ignore
+        window.PointerEvent = class extends MouseEvent {
+            constructor(type: string, eventInitDict: MouseEventInit) {
+                super(type, eventInitDict);
+            }
+        };
+        outsideElement.dispatchEvent(mouseDownEvent);
+
+        expect(onOutsideClick).toHaveBeenCalled();
+
+        outsideElement.dispatchEvent(mouseDownEvent);
+        expect(onOutsideClick).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call onOutsideClick when touchstart outside the specified element', () => {
+        const elementId = 'testElement';
+        const onOutsideClick = jest.fn();
+
+        outsideClick(elementId, onOutsideClick);
+
+        jest.advanceTimersByTime(0);
+
+        const outsideElement = document.body;
+        const touchStartEvent = new TouchEvent('touchstart', { bubbles: true });
+        outsideElement.dispatchEvent(touchStartEvent);
+
+        expect(onOutsideClick).toHaveBeenCalled();
+
+        outsideElement.dispatchEvent(touchStartEvent);
+        expect(onOutsideClick).toHaveBeenCalledTimes(1);
     });
 });
