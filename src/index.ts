1095837a2f2ae7ebd130d03f07e53535402149c9
Kembec <account@kembec.com>
2024-11-23 17:09:26 -0500

    update: click => pointerdown

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 function outsideClick(elementId: string, onOutsideClick: () => void, timeout: number = 0): void {
-	const handleOutsideClick = (event: MouseEvent) => {
-		const specifiedElement = document.getElementById(elementId);
-		
-		if (specifiedElement && !specifiedElement.contains(event.target as Node)) {
-			onOutsideClick();
-			document.removeEventListener("click", handleOutsideClick); 
-		}
-	};
-
-	setTimeout(() => document.addEventListener("click", handleOutsideClick), timeout);
+    const handleOutsideClick = (event: Event) => {
+        const specifiedElement = document.getElementById(elementId);
+        if (specifiedElement && !specifiedElement.contains(event.target as Node)) {
+            onOutsideClick();
+            document.removeEventListener("pointerdown", handleOutsideClick);
+            document.removeEventListener("touchstart", handleOutsideClick);
+        }
+    };
+    setTimeout(() => {
+        document.addEventListener("pointerdown", handleOutsideClick);
+        document.addEventListener("touchstart", handleOutsideClick);
+    }, timeout);
 }
 
 export { outsideClick };
